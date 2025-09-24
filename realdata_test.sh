bab06c30f0eb2254d22be4c53b4190afe0c0991b
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-09-24 20:49:57 +0700

    Update realdata_test.sh

--- realdata_test.sh
+++ realdata_test.sh
@@ -1,8 +1,9 @@
-REALDATA_QUERY_VER=v1 \
-python test.py \
+python eval_all.py \
   --config_file configs/VeRi/vit_base.yml \
-  DATASETS.NAMES realdata \
-  DATASETS.ROOT_DIR ./ \
-  TEST.WEIGHT transformer_120.pth \
-  OUTPUT_DIR logs/test_realdata_v2q \
-  MODEL.DEVICE_ID "('0')"
+  --root_dir /kaggle/working \
+  --weight /kaggle/input/temp/pytorch/default/1/transformer_120.pth \
+  --query_ver v1 \
+  --device 0 \
+  --ims_per_batch 32 \
+  --num_workers 2 \
+  --neck_feat after
