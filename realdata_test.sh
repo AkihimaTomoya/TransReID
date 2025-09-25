8db3566b6d2ee25a1fede6b7546afa91b6756d3b
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-09-25 17:14:33 +0700

    Update realdata_test.sh

--- realdata_test.sh
+++ realdata_test.sh
@@ -1,9 +1,10 @@
-python eval_all_gocs.py \
+python eval_all.py \
   --config_file configs/VeRi/vit_base.yml \
-  --root_dir /kaggle/working \
-  --weight /kaggle/input/temp/pytorch/default/1/transformer_120.pth \
+  --root_dir ./ \
+  --weight transformer_120.pth \
   --query_ver v1 \
   --device 0 \
   --ims_per_batch 32 \
   --num_workers 2 \
-  --neck_feat after
+  --neck_feat before \
+  --tta_flip
